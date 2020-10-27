2533a97b66f3ebedd74979bedf55095f5226ce6c
Kamil Kuliś <k.kuliss@gmail.com>
2020-10-27 23:50:01 +0100

    Initialize setup

--- js/index.js
+++ js/index.js
@@ -1,14 +1,24 @@
 class Slider {
     constructor(images){
-        this.images = images
+        this.images = images;
+        this.slide = null;
+        this.prevBtn = null;
+        this.nextBtn = null;
+
 
         this.UiSelectors = {
-            slide: '[date]'
+            slide: '[date-slide]',
+            buttonPrev: '[data-button-prev]',
+            buttonNext: '[data-button-next]'
         
-    }
+    } }
+
 
 
     initializeSlider() {
 
+            this.slide = document.querySelector(this.UiSelectors.slide);
+            this.prevBtn = document.querySelector(this.UiSelectors.buttonPrev);
+            this.buttonNext = document.querySelector(this.UiSelectors.buttonNext);
     }
 }
